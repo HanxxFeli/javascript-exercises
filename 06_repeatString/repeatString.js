fa8529cd1137bdeb6d413663c059f046587ddb6c
Hans Feliciano <hgvfeliciano10@gmail.com>
2025-05-10 14:29:45 -0600

    "added solution to function"

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(input, count) {
+    let initialCount = 0;
+    let output = "";
 
+    while (initialCount < count) { 
+        output += input;
+        initialCount++;
+    }
+
+    return output;
 };
 
 // Do not edit below this line
