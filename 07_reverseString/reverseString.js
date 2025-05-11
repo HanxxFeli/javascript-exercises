de44cb604f67c9cfbe7fd2b1d4a1289b519824f5
Hans Feliciano <hgvfeliciano10@gmail.com>
2025-05-11 14:24:07 -0600

    "solved problem 7"

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    // add to a string
+    let output = "";
 
+    // split the string 
+    let letters = input.split("");
+    
+    // return added string
+    for (let i = letters.length-1; i >= 0; i--) { 
+        output += letters[i];
+    }
+
+    return output
 };
 
+console.log(reverseString("hello there"));
 // Do not edit below this line
 module.exports = reverseString;
