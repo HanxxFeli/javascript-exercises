c32672dfda36f64fd05d8add68491516a64e0556
Hans Feliciano <hgvfeliciano10@gmail.com>
2025-05-13 14:43:32 -0600

    added initial solution to problem 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    for (i = start; i <= end; i++) { 
+        sum += i;
+    }
+
+    return sum;
 };
+
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
